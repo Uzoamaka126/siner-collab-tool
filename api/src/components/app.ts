1b2166d01b4ba6e2895de4a399485c66134dd40c
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2021-07-23 09:26:07 +0100

    install necessary packages and their corresponding types

--- api/src/components/app.ts
+++ api/src/components/app.ts
@@ -1,13 +1,74 @@
 // Entry point for all libraries
-// Configured port here
-
+// Import necessary configured ports here
+import { CLIENT_URL, mongoURI, NODE_ENV, port } from "./../config/index";
 // import necessary packages
 import express from 'express'
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
-
+import morgan from "morgan";
+import mongoose from "mongoose";
 // import router files here
 
 
 // import error handler file
+
+// connect to mongoose
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((conn) => {
+    app.set("db_connection", conn);
+    console.log(
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      `MongoDB connection with url successful @: ${conn.connection.host}:${conn.connection.port}`
+    );
+  })
+  .catch((err) => {
+    console.log(err, "This shouldn't be happening");
+  });
+
+const apiRouter = express.Router();
+// apiRouter.use("/auth", authRouter);
+// apiRouter.use("/tasks", tasksRouter);
+// apiRouter.use("/workspaces", workspaceRouter);
+
+const app = express();
+
+app.set("port", port);
+app.use(helmet());
+if (!(NODE_ENV === "dev" || NODE_ENV === "test")) {
+  app.use(morgan("tiny"));
+}
+app.use(
+  cors({
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+    origin: CLIENT_URL,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+// use express on the router
+app.use("/api", apiRouter);
+
+// use express to do a get request
+app.get("/", (_, res: express.Response) => {
+  return res.status(200).json({ message: "API is up 🚀" });
+});
+
+// setup the error handler here
+// if (NODE_ENV === "dev" || NODE_ENV === "test") {
+//   app.use(errorHandler());
+// }
+
+app.all("*", (_, res: express.Response) => {
+  res.status(404).json({ message: "This URL can not be found!" });
+});
+
+export default app;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,41 @@
-const app = express()
+import dotenv from "dotenv";
+
+// import app module
+import app from './components/app';
+
+dotenv.config();
+
+export function bootstrap(): void {
+  try {
+    // init db connection
+
+    /**
+     * Start Express server.
+     */
+    app.listen(app.get("port"), () => {
+      console.log(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `App is running at http://localhost:${app.get("port")} in ${app.get(
+          "env"
+        )} mode`
+      );
+      console.log("  Press CTRL-C to stop\n");
+    });
+  } catch (error) {
+    console.log(error, "This shouldn't be happening");
+  }
+}
+
+bootstrap();
+
 const port = 5000
 app.get('/', (_, res) => {
   res.status(200).send()
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
+
+// export * from "./utils/typeUtils";
+// export * from "./components/auth/middleware";
+// export * from "./components/auth/validation";
+// export { registerInput } from "./components/auth/controller";
+// export { getInviteData } from "./publicTypes/index";
