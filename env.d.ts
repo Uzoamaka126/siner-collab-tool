f790e2005f413b7c857d05a9ce1b501bc1ff2bf9
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2021-07-27 00:55:51 +0100

    add env to gitignore

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends Dict<string> {
+    interface ProcessEnv extends Dictionary<string> {
       JWT_SECRET?: string;
       NODE_ENV: "test" | "prod" | "dev";
       DB_CONNECTION_TEST?: string;
@@ -9,6 +9,7 @@ declare global {
       EMAIL_PASSWORD?: string;
       EMAIL_SECRET?: string;
       CLIENT_URL?: string;
+      PORT?: number;
     }
   }
 }
