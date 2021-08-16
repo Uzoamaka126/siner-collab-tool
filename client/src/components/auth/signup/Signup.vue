0ce4a68f85b3f54c5e74e66ddc141d01c9449630
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2021-08-16 09:53:13 +0100

    install axios, vuex and node sass

---
+++ client/src/components/auth/signup/Signup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="auth-wrap">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Signup',
+//   props: {
+//     msg: {
+//       type: String,
+//       required: false,
+//     },
+//   },
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/router/index.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+export const router = createRouter({
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: Signup
+        }
+    ],
+    history: createWebHashHistory(),
+    linkActiveClass: 'active'
+});
+
+const app = createApp({ /* your root component */ });
+app.use(router)
