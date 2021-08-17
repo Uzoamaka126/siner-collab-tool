23515fcd2e8d87d4c8463a416ca86bcc92080133
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2021-08-17 20:42:21 +0100

    setup auth files and routes

--- client-app/src/router/index.js
+++ client-app/src/router/index.js
@@ -1,25 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue';
+import LoginView from '../views/Login.vue';
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+            path: '/',
+            name: 'home',
+            redirect: { name: 'login' }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: Signup
+        }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  linkActiveClass: 'active'
 })
 
 export default router
